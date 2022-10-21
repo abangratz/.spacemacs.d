1a4eb78f1d46a4cac36b92513b7822bf9e6ac4af
Anton Bangratz <anton@platogo.com>
2022-10-21 14:13:20 +0200

    Add: spacemacs buffer recents

--- init.el
+++ init.el
@@ -74,7 +74,7 @@ Check `dotspacemacs/get-variable-string-list' for all vars you can configure."
    dotspacemacs-pretty-docs                t
    dotspacemacs-search-tools               '("ag" "rg" "pt" "ack" "grep")
    dotspacemacs-scratch-mode               'org-mode
-   dotspacemacs-startup-lists              nil
+   dotspacemacs-startup-lists              '((recents . 10))
    dotspacemacs-whitespace-cleanup         'trailing
 
    ;; The following are unchanged but are still required for reloading via
